83a28043770bc5610ffe37dea0b80b835ef4a9d7
Sumedh <sbarsagade248@gmail.com>
2024-05-14 16:59:48 +0530

    updated url in index.js file for fecth err

--- backend/index.js
+++ backend/index.js
@@ -9,14 +9,14 @@ const router = require('./routes')
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:3000/",
+    origin: "http://localhost:3000",
     credentials : true
 }))
 
-app.use(cors({
-    origin : process.env.FRONTEND_URL,
-    credentials : true
-}))
+// app.use(cors({
+//     origin : process.env.FRONTEND_URL,
+//     credentials : true
+// }))
 app.use(express.json())
 app.use(cookieParser())
 
